9d0267ca9908b5bfd95aee4e32da5bc26e771d84
rimatla <alt.coelho@gmail.com>
2017-08-19 15:40:32 -0500

    react router additions

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header';
 import Shop from './components/Shop/Shop';
+import OrderReview from './components/OrderReview/OrderReview';
+import NotFound from './components/NotFound/NotFound';
+
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 class App extends Component {
   render() {
+    //pass in routes
     return (
       <div className="App">
           <Header/>
-          <Shop/>
+          <BrowserRouter>
+              <Switch>
+                  <Route exact path="/" component={Shop}></Route>
+                  <Route path="/shop" component={Shop}></Route>
+                  <Route path="/review" component={OrderReview}></Route>
+                  <Route path="*" component={NotFound}></Route>
+              </Switch>
+          </BrowserRouter>
       </div>
     );
   }
 }
 
 export default App;
+
+/*<Shop/>*/
---
+++ src/components/OrderReview/OrderReview.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+class OrderReview extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Order Review</h1>
+            </div>
+        )
+    }
+}
+
+export default OrderReview ;
